53d4f7fa7c406a9cf69e1423ad612c7f99d15e64
Guiphb <gui.phb@gmail.com>
2021-04-24 20:59:34 -0300

    Cleaning PatientComponent code

--- healthin-app/src/app/patient/components/patient/patient.component.ts
+++ healthin-app/src/app/patient/components/patient/patient.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { debounceTime, map } from 'rxjs/operators';
+import { debounceTime, map, switchMap } from 'rxjs/operators';
 import { Patient } from 'src/app/shared/interfaces/patient';
 import { PatientService } from 'src/app/shared/services/patient.service';
 
@@ -15,7 +15,7 @@ export class PatientComponent implements OnInit {
   patientId: string;
   navLinks: any[];
   formGroup: FormGroup;
-  patientOptions$: Observable<Patient[]>;
+  patients$: Observable<Patient[]>;
   patientSelected: Patient;
 
   constructor(private patientService: PatientService) {
@@ -38,24 +38,12 @@ export class PatientComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.patientOptions$ = this.formGroup
+    this.patients$ = this.formGroup.get('patientName')
     .valueChanges.pipe(
       debounceTime(1000), // Waits 1 second to the last request, to make the request with the most recent value
-      map(value =>
-        this.filter(
-          typeof(value.patientName) === 'string' &&
-          value.patientName ? value.patientName : ''
-        )
-      )
+      map(value =>  typeof(value) === 'string' && value ? value : ''),
+      switchMap(name => this.patientService.getByName(name))
     );
-  }
-
-  private filter(value: string): Patient[] {
-    let patients: Patient[] = null;
-    this.patientService.getByName(value).subscribe(res => {
-      patients = res;
-    });
-    return patients;
   }
 
   getOptionName(patient: Patient): string {
